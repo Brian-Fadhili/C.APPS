de025e5a819a6adb2edcdb1e50009963ef5fd3aa
Brian Fadhili <brianfadhili05@gmail.com>
2024-02-02 09:44:15 +0300

    assignment01

---
+++ 01assignment.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int celsius, b, fahrenheit;
+    celsius = 20;
+    b = 32;
+    fahrenheit = celsius + b;
+    printf("Temp in Fahrenheit = %d\n", fahrenheit);
+}
